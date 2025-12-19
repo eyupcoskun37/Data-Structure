7cf1a9f280289ca8b852fd44d31afa2951960c1e
Eyüp Coşkun <151372999+eyupcoskun37@users.noreply.github.com>
2025-12-19 11:58:31 +0300

    Create 11.week_Palindrome.c

---
+++ 11.week_Palindrome.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+char stack[100];
+int top = -1;
+
+void push(char c)
+{
+    stack[++top] = c;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int main()
+{
+    char str[100];
+    int i, len, flag = 1;
+
+    printf("Kelime giriniz: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    for (i = 0; i < len / 2; i++)
+        push(str[i]);
+
+    if (len % 2 != 0)
+        i++;  // ortadaki harfi atla
+
+    for (; i < len; i++)
+    {
+        if (str[i] != pop())
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("Palindrome\n");
+    else
+        printf("Palindrome degil\n");
+
+    return 0;
+}
